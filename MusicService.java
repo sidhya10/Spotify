42e5c04083b5e0370bc51e156b014b754600d7f2
sidhya10 <163345433+sidhya10@users.noreply.github.com>
2024-03-13 19:19:48 -0400

    Add files via upload

---
+++ MusicService.java
@@ -0,0 +1,23 @@
+//Sidhya Jeyaram, 115735772
+import java.util.ArrayList;
+public class MusicService {
+
+    private static ArrayList<User> users = new ArrayList<User>();
+
+    public static void addUser(User user){
+        users.add(user);
+    }
+    public void removeUser(String userName){
+        for (int i = 0; i < users.size(); i++)
+            if (users.get(i).getUsername().equals(userName)) {
+                users.remove(i);
+                return;
+            }
+    }
+    public static ArrayList<Playlist> getUserPlaylists(User user){
+        for (int i = 0; i < users.size(); i++)
+            if (users.get(i).getUsername().equals(user.getUsername()))
+                return users.get(i).getPlaylists();
+        return new ArrayList<Playlist>();
+    }
+}
